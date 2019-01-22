fa823cfe1637b94b98dc95e26d4f3f3fc1043f18
Meredith <meredityman@gmail.com>
2019-01-22 13:06:09 +0100

    Tweaked config

--- addon_config.mk
+++ addon_config.mk
@@ -1,8 +1,8 @@
 meta:
 	ADDON_NAME = ofxPixelSorter
-	ADDON_DESCRIPTION = A threaded pixel sorter
+	ADDON_DESCRIPTION = A threaded, configurable pixel sorter of 
 	ADDON_AUTHOR = @meredityman
-	ADDON_TAGS = "addon" "pixel sorting"
+	ADDON_TAGS = "addon" "pixel sorting" "graphics"
 	ADDON_URL = http://github.com/meredityman/ofxPixelSorter
 
  
